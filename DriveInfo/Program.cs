512d5ab88382d8303288a53ab39f38215f40a3b9
Billy Tukija Richard <billytukija@gmail.com>
2019-10-31 22:39:18 -0300

    Wroking with DriveInfo

---
+++ DriveInfo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace DriveInfos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] driveInfos = DriveInfo.GetDrives();
+
+            foreach (var drive in driveInfos)
+            {
+                Console.WriteLine("Name : " + drive.Name);
+                Console.WriteLine("Total Size : " + drive.TotalSize);
+                Console.WriteLine("Total Free Space : " + drive.TotalFreeSpace);
+                Console.WriteLine("Drive Type : " + drive.DriveType);
+                Console.WriteLine("Volume Label : " + drive.DriveType);
+                Console.WriteLine("Root Directory : " + drive.RootDirectory);
+                Console.WriteLine("Drive Format : " + drive.DriveFormat);
+            }
+        }
+    }
+}
