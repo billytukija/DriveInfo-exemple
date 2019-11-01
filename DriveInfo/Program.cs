7b7d57bf3b9eb008e89169183b46642f8c2ecb0e
Billy Tukija <billytukij@gmail.com>
2019-11-01 02:46:48 -0300

    Separação

--- DriveInfo/Program.cs
+++ DriveInfo/Program.cs
@@ -15,6 +15,7 @@ namespace DriveInfos
                 Console.WriteLine("Total Size : " + drive.TotalSize);
                 Console.WriteLine("Total Free Space : " + drive.TotalFreeSpace);
                 Console.WriteLine("Drive Type : " + drive.DriveType);
+
                 Console.WriteLine("Volume Label : " + drive.DriveType);
                 Console.WriteLine("Root Directory : " + drive.RootDirectory);
                 Console.WriteLine("Drive Format : " + drive.DriveFormat);
